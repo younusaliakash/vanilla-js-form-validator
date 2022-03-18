c5bae015a9b3903be7fcd214eb5b2d14bdca9e0b
Younusaliakash <younusaliakash8@gmail.comm>
2022-03-18 18:16:55 +0600

    required validation done

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
+const form  = document.getElementById("form");
+const username  = document.getElementById("username");
+const email  = document.getElementById("email");
+const password  = document.getElementById("password");
+const password2  = document.getElementById("password2");
 
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector('small');
+  small.innerText = message 
+
+}
+
+const showSuccess = (input) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+
+}
+
+
+//Event listener
+form.addEventListener("submit", function(e){
+  e.preventDefault()
+
+  if(username.value === ''){
+      showError(username, "Username is required!")
+  }else{
+    showSuccess(username)
+  }
+
+  if(email.value === ''){
+      showError(email, "email is required!")
+  }else{
+    showSuccess(email)
+  }
+
+  if(password.value === ''){
+      showError(password, "password is required!")
+  }else{
+    showSuccess(password)
+  }
+
+  if(password2.value === ''){
+      showError(password2, "password2 is required!")
+  }else{
+    showSuccess(password2)
+  }
+})
 
 
 
